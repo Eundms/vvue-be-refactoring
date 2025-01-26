72404ad59c7f33ee93943760be6a75b69130b99c
Eundms <dms011221@gmail.com>
2025-01-26 15:42:38 +0900

    [chore] 커스텀 로깅 결과 표시

--- src/main/java/com/exciting/vvue/common/logging/LoggingAspect.java
+++ src/main/java/com/exciting/vvue/common/logging/LoggingAspect.java
@@ -1,9 +1,8 @@
 package com.exciting.vvue.common.logging;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.UUID;
-
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -12,10 +11,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Aspect
 @Component
 public class LoggingAspect {
 	private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+	private final MeterRegistry meterRegistry;
+
+	public LoggingAspect(MeterRegistry meterRegistry) {
+		this.meterRegistry = meterRegistry;
+	}
 
 	private static final ThreadLocal<String> requestIdThreadLocal = new ThreadLocal<>();
 	private static final ThreadLocal<Integer> callDepthThreadLocal = ThreadLocal.withInitial(() -> 0);
@@ -39,51 +46,29 @@ public class LoggingAspect {
 		int callDepth = callDepthThreadLocal.get();
 		callDepthThreadLocal.set(callDepth + 1);
 
-		// 접두사 생성
-		String entryPrefix = "=".repeat(callDepth) + "=>";
-		String exitPrefix = "<=" + "=".repeat(callDepth);
-
 		String requestId = requestIdThreadLocal.get();
-		String currentTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
 		String pointcutName = resolvePointcutName(proceedingJoinPoint);
 
-		logger.info("[{}] [{}] {} [{}] {} | started with arguments: {}",
-			requestId,
-			currentTime,
-			entryPrefix,
-			pointcutName,
-			proceedingJoinPoint.getSignature().toShortString(),
-			proceedingJoinPoint.getArgs());
+		String methodName = proceedingJoinPoint.getSignature().toShortString();
+		Timer timer = meterRegistry.timer("method_execution_time", "method", methodName, "type", pointcutName);
+		Counter errorCounter = meterRegistry.counter("method_execution_error", "method", methodName, "type", pointcutName);
 
 		long startTime = System.nanoTime();
 
+		logger.info("[{}] {} started with arguments: {}", requestId, methodName, proceedingJoinPoint.getArgs());
 		try {
-			Object result = proceedingJoinPoint.proceed();
-			String afterCurrentTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
-			long duration = (System.nanoTime() - startTime) / 1_000_000; // 밀리초
-
-			logger.info("[{}] [{}] {} [{}] {} | completed in {} ms. Return value: {}",
-				requestId,
-				afterCurrentTime,
-				exitPrefix,
-				pointcutName,
-				proceedingJoinPoint.getSignature().toShortString(),
-				duration,
-				result);
-
-			return result;
+			// 실행 시간 기록
+			return timer.recordCallable(() -> {
+				try {
+					return proceedingJoinPoint.proceed();
+				} catch (Throwable e) {
+					throw new RuntimeException(e);
+				}
+			});
 		} catch (Throwable throwable) {
-			String afterCurrentTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
-			long duration = (System.nanoTime() - startTime) / 1_000_000; // 밀리초
-
-			logger.error("[{}] [{}] {} [{}] {} | threw exception after {} ms: {}",
-				requestId,
-				afterCurrentTime,
-				exitPrefix,
-				pointcutName,
-				proceedingJoinPoint.getSignature().toShortString(),
-				duration,
-				throwable.getMessage());
+			// 에러 카운트 증가
+			errorCounter.increment();
+			logger.error("[{}] {} threw exception: {}", requestId, methodName, throwable.getMessage());
 			throw throwable;
 		} finally {
 			callDepthThreadLocal.set(callDepthThreadLocal.get() - 1);
@@ -92,6 +77,9 @@ public class LoggingAspect {
 				requestIdThreadLocal.remove();
 				callDepthThreadLocal.remove();
 			}
+
+			long duration = (System.nanoTime() - startTime) / 1_000_000; // 밀리초
+			logger.info("[{}] {} completed in {} ms", requestId, methodName, duration);
 		}
 	}
 
