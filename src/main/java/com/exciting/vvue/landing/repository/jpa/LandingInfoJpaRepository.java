995a4c3677b686396c6ce1259bf94610aa99480b
eunjeong Park <eunjeongpark.eundms@gmail.com>
2025-08-03 01:10:16 +0900

    refactor: 랜딩 페이지 레포지토리 개선

--- src/main/java/com/exciting/vvue/landing/repository/jpa/LandingInfoJpaRepository.java
+++ src/main/java/com/exciting/vvue/landing/repository/jpa/LandingInfoJpaRepository.java
@@ -8,17 +8,16 @@ import org.springframework.data.repository.query.Param;
 
 public interface LandingInfoJpaRepository extends JpaRepository<User, Long> {
 
-  @Query("SELECT new com.exciting.vvue.landing.model.dto.LandingInfos(" +
-      "u.id, " +
-      "CASE " +
-      "   WHEN u.gender IS NOT NULL AND u.birthday IS NOT NULL AND u.nickname IS NOT NULL THEN true "
-      +
-      "   ELSE false " +
-      "END, " +
-      "COALESCE(m.id, null), " +
-      "COALESCE(m.marriedDay, null)) " +
-      "FROM User u " +
-      "LEFT JOIN Married m ON m.first.id = u.id OR m.second.id = u.id " +
-      "WHERE u.id = :userId")
+  @Query("SELECT new com.exciting.vvue.landing.model.dto.LandingInfos("
+      + "u.id, "
+      + "CASE "
+      + "   WHEN u.gender IS NOT NULL AND u.birthday IS NOT NULL AND u.nickname IS NOT NULL THEN true "
+      + "   ELSE false "
+      + "END, "
+      + "COALESCE(m.id, null), "
+      + "COALESCE(m.marriedDay, null)) "
+      + "FROM User u "
+      + "LEFT JOIN Married m ON m.first.id = u.id OR m.second.id = u.id "
+      + "WHERE u.id = :userId")
   LandingInfos findUserRelatedInfo(@Param("userId") Long userId);
 }
